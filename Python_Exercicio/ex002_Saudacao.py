0ff708f8ebe8d5eb630edd7c600c7b5a5292b6ba
codeIsrael <62953554+codeIsrael@users.noreply.github.com>
2021-02-27 21:45:58 -0300

    Update ex002_Saudacao.py
    arrumando o codigo

--- Python_Exercicio/ex002_Saudacao.py
+++ Python_Exercicio/ex002_Saudacao.py
@@ -2,11 +2,12 @@
 Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 """
 
-name = input("Qual o seu nome? ").title()
-
 
 def boas_vindas(name):
     return f"Seja muito bem vindo {name}"
 
 
-print(boas_vindas(name))
+nome = input("Qual o seu nome? ").title()
+
+
+print(boas_vindas(nome))
