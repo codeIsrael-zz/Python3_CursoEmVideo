a2471ff851a4a177f907c75d9546b1bd3a1369a5
codeIsrael <62953554+codeIsrael@users.noreply.github.com>
2021-02-27 21:42:58 -0300

    ex003
    Resolução ex003

---
+++ Python_Exercicio/ex003_Soma_Num.py
@@ -0,0 +1,13 @@
+"""
+Crie um programa que leia dois numeros e retorne a soma entre eles.
+"""
+
+
+def soma(x, y):
+    return x + y
+
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+
+print(soma(n1, n2))
